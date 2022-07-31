06f3913855e90bb2f720833f2c31d95443343d97
atiQue <atiqurrahman7465@gmail.com>
2022-07-31 20:53:19 +0600

    [Arduino with Java] - exercise 11.

---
+++ src/lab11/Lab11Part2.java
@@ -0,0 +1,47 @@
+package lab11;
+
+import jm.JMC;
+import jm.music.data.Note;
+import jm.util.Play;
+
+/**
+ * @author atiQue
+ * @since 28'Jul 2022 at 10:32 PM
+ */
+
+public class Lab11Part2 {
+    public static void main(String[] args) {
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.B4, JMC.TN));
+        Play.midi(new Note(JMC.B5, JMC.TN));
+        Play.midi(new Note(JMC.B6, JMC.TN));
+        Play.midi(new Note(JMC.BF4, JMC.TN));
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.A1, JMC.TN));
+        Play.midi(new Note(JMC.C4, JMC.TN));
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.B4, JMC.TN));
+        Play.midi(new Note(JMC.B5, JMC.TN));
+        Play.midi(new Note(JMC.B6, JMC.TN));
+        Play.midi(new Note(JMC.BF4, JMC.TN));
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.A1, JMC.TN));
+        Play.midi(new Note(JMC.C4, JMC.TN));
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.B4, JMC.TN));
+        Play.midi(new Note(JMC.B5, JMC.TN));
+        Play.midi(new Note(JMC.B6, JMC.TN));
+        Play.midi(new Note(JMC.BF4, JMC.TN));
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.A1, JMC.TN));
+        Play.midi(new Note(JMC.C4, JMC.TN));
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.B4, JMC.TN));
+        Play.midi(new Note(JMC.B5, JMC.TN));
+        Play.midi(new Note(JMC.B6, JMC.TN));
+        Play.midi(new Note(JMC.BF4, JMC.TN));
+        Play.midi(new Note(JMC.B3, JMC.TN));
+        Play.midi(new Note(JMC.A1, JMC.TN));
+        Play.midi(new Note(JMC.C4, JMC.TN));
+    }
+}
